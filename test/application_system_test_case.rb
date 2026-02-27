a95aa44e604fd54fe62516893a76c2b1b58677a5
Rida Al Barazi <1444+rbarazi@users.noreply.github.com>
2026-02-27 15:29:26 +0000

    Fallback to local headless system driver when remote Selenium is unset

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,12 +1,16 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  Capybara.server_host = "0.0.0.0"
-  Capybara.server_port = 40_001
-  Capybara.app_host = "http://rails-app:#{Capybara.server_port}"
+  if ENV["SELENIUM_DRIVER_URL"].present?
+    Capybara.server_host = "0.0.0.0"
+    Capybara.server_port = 40_001
+    Capybara.app_host = ENV.fetch("CAPYBARA_APP_HOST", "http://rails-app:#{Capybara.server_port}")
 
-  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ], options: {
-    browser: :remote,
-    url: ENV.fetch("SELENIUM_DRIVER_URL", "http://chrome:4444")
-  }
+    driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ], options: {
+      browser: :remote,
+      url: ENV.fetch("SELENIUM_DRIVER_URL")
+    }
+  else
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  end
 end
